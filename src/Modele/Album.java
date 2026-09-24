0f64efd15bb934860188cdb1f3579d74205c1aa5
Helessen <chauveau.vincent@gmail.com>
2026-09-24 12:25:56 +0200

    2 classes Album et CompactDisque

--- src/Modele/Album.java
+++ src/Modele/Album.java
@@ -1,3 +1,17 @@
 package Modele;
-public class Album {
+
+import java.util.Date;
+
+public abstract class Album {
+    String nom;
+    String auteur;
+    Date sortie;
+    int quantite;
+
+    public Album(String nom, String auteur, Date sortie, int quantite) {
+        this.nom = nom;
+        this.auteur = auteur;
+        this.sortie = sortie;
+        this.quantite = quantite;
+    }
 }
--- src/Modele/CompactDisque.java
+++ src/Modele/CompactDisque.java
@@ -1,4 +1,38 @@
 package Modele;
 
-public class CompactDisque {
+import java.util.Date;
+
+public class CompactDisque extends Album{
+    int numero;
+    int type;
+
+    public CompactDisque(String nom, String auteur, Date sortie, int quantite, int numero, int type) {
+        super(nom, auteur, sortie, quantite);
+        this.numero = numero;
+        this.type = type;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "CompactDisque{" +
+                "numero=" + numero +
+                ", type=" + type +
+                '}';
+    }
 }
