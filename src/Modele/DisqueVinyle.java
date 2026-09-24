3de2e24d098c7e6511c1972525ae0ae2920d04a1
Yanis Person <yanisperson7@gmail.com>
2026-09-24 14:07:08 +0200

    correction DisqueVinyle + FichierNumerique

--- src/Modele/DisqueVinyle.java
+++ src/Modele/DisqueVinyle.java
@@ -1,4 +1,37 @@
 package Modele;
+import java.util.Date;
 
-public class DisqueVinyle {
+public class DisqueVinyle extends Album {
+    private String numero;
+    private int taille;
+
+    public DisqueVinyle(String nom, String auteur, Date sortie, int quantite, String numero, int taille) {
+        super(nom, auteur, sortie, quantite);
+        this.numero = numero;
+        this.taille = taille;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public int getTaille() {
+        return taille;
+    }
+
+    public void setTaille(int taille) {
+        this.taille = taille;
+    }
+
+    @Override
+    public String toString() {
+        return "DisqueVinyle{" +
+                "numero=" + numero +
+                ", taille=" + taille +
+                '}';
+    }
 }
--- src/Modele/FichierNumerique.java
+++ src/Modele/FichierNumerique.java
@@ -1,4 +1,44 @@
 package Modele;
 
-public class FichierNumerique {
+public class FichierNumerique extends Album {
+    private String format;
+    private double taille;
+    private int duree;
+
+
+
+    public String getFormat() {
+        return format;
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public double getTaille() {
+        return taille;
+    }
+
+    public void setTaille(double taille) {
+        this.taille = taille;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    @Override
+    public String toString() {
+        return "FichierNumerique{" +
+                "format='" + format + '\'' +
+                ", taille=" + taille +
+                ", duree=" + duree +
+                '}';
+    }
+
+
 }
