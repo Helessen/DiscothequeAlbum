20ff5889cae630a0a567fc6ab833d528543406ee
Helessen <chauveau.vincent@gmail.com>
2026-09-24 15:00:08 +0200

    petit ajout Album et CD
    abstract class + modif int to String (numero)

--- src/Modele/CompactDisque.java
+++ src/Modele/CompactDisque.java
@@ -3,20 +3,24 @@ package Modele;
 import java.time.LocalDate;
 
 public class CompactDisque extends Album{
-    private int numero;
+    private String numero;
     private String type;
 
-    public CompactDisque(String nom, String auteur, LocalDate sortie, int quantite, int numero, String type) {
+    public CompactDisque(String nom, String auteur, LocalDate sortie, int quantite, String numero, String type) {
         super(nom, auteur, sortie, quantite);
         this.numero = numero;
         this.type = type;
     }
 
-    public int getNumero() {
+    public String getSupport() {
+        return "CD";
+    }
+
+    public String getNumero() {
         return numero;
     }
 
-    public void setNumero(int numero) {
+    public void setNumero(String numero) {
         this.numero = numero;
     }
 
